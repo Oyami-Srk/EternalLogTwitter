4ffef92d63ea9a46b6e929480afdb491164e92cc
Shiroko <hhx.xxm@gmail.com>
2024-09-09 00:15:53 +0800

    Add method to allow post multiple urls

--- src/api/task.py
+++ src/api/task.py
@@ -12,42 +12,80 @@ router = APIRouter()
 
 
 class NewTask(BaseModel):
-    url: AnyHttpUrl
+    url: AnyHttpUrl | list[AnyHttpUrl]
 
 
-@router.post("/task")
-async def create_new_task(task: NewTask, db: Session = Depends(get_db)):
-    url = ""
-    if task.url.host in URLTransformers:
-        url = URLTransformers[task.url.host].transform(str(task.url))
+async def process_one_url(url: AnyHttpUrl, db: Session) -> tuple[str, Task | CompletedTask | None]:
+    original_url = url
+    if url.host in URLTransformers:
+        url = URLTransformers[url.host].transform(str(url))
     else:
-        url = str(task.url)
+        url = str(url)
 
     existing = db.query(Task).where(Task.url == url).first()  # type: ignore
     existing = existing if existing is not None else db.query(CompletedTask).where(
         CompletedTask.url == url).first()  # type: ignore
 
     if existing is not None:  # type: ignore
-        return {
-            "success": False,
-            "message": "Task already exists",
-            "task": {
-                "id": existing.id,
-                "url": url,
-                "type": "completed" if isinstance(existing, CompletedTask) else "pending"
-            }
-        }
+        return url, existing
 
-    db.add(Task(url=url, original_url=str(task.url)))
+    db.add(Task(url=url, original_url=str(original_url)))
     db.commit()
 
-    return {
-        "success": True,
-        "message": "Task created successfully",
-        "task": {
-            "url": url,
+    return url, None
+
+
+@router.post("/task")
+async def create_new_task(task: NewTask, db: Session = Depends(get_db)):
+    if isinstance(task.url, list):
+        existed = []
+        new = []
+        for url in task.url:
+            url, existing = await process_one_url(url, db)
+            if existing is not None:
+                existed.append({
+                    "id": existing.id,
+                    "url": url,
+                    "type": "completed" if isinstance(existing, CompletedTask) else "pending"
+                })
+            else:
+                new.append(url)
+        return {
+            "status": "ok",
+            "success": True,
+            "message": "Batch task created successfully",
+            "tasks": {
+                "new": new,
+                "existed": {
+                    int(e['id']): {
+                        "type": e['type'],
+                        "url": e['url']
+                    } for e in existed
+                }
+            }
         }
-    }
+    else:
+        url, existing = await process_one_url(task.url, db)
+        if existing is not None:
+            return {
+                "status": "ok",
+                "success": False,
+                "message": "Task already exists",
+                "task": {
+                    "id": existing.id,
+                    "url": url,
+                    "type": "completed" if isinstance(existing, CompletedTask) else "pending"
+                }
+            }
+        else:
+            return {
+                "status": "ok",
+                "success": True,
+                "message": "Task created successfully",
+                "task": {
+                    "url": url,
+                }
+            }
 
 
 @router.get("/task/{task_id}")
